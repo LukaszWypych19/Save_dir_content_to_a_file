b9852c9cd66b0ca0d053267b92868d54a7f2b16b
Luk <lukasz.wypych19@gmail.com>
2023-04-07 10:15:44 +0200

    working script to save the contents of a directory in a file - plus list of file size

--- dir_content.py
+++ dir_content.py
@@ -19,16 +19,30 @@ import os
 
 
 print(os.getcwd())
+
+                    # podajemy sciezke do katalogu z ktorego chcemy pobrac nazwy pliow / katalogow
 os.chdir('D:\\Python\\Moje projekty\\Dir_content_in_file\\test_dir')
 print(os.getcwd())
 
+                    # printujemy liste plikow i/lub katalogow w danej lokalizacji
 list_of_files = os.listdir()
 print(list_of_files)
 
-plik = open("test_file_02.txt", 'w+')
-
+            # tworzymy liste rozmiarow dla poszczegolnych plikow
+size_list = []
 for i in list_of_files:
-    plik.write(i + '\n')
+    size = (os.path.getsize(i))
+    size_list.append(size)
+print(size_list)
+
+
+                       #podajemy sciezke gdzie ma sie zapisac plik z nazwami plikow / katalogow
+
+plik = open("D:\\Python\\Moje projekty\\Dir_content_in_file\\test_dir\\test_file_02.txt", 'w+')
+
+                        # zapisuje w pliku elementy listy jeden pod drugim
+for i in list_of_files:
+    plik.write(i + "\n")
 
 plik.close()
 
